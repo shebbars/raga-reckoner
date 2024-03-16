5506fa6353bdeffd19badea2b7f8da9065c3d45b
Subramanya Hebbar <shebbars@yahoo.com>
2024-03-16 18:44:47 +0530

    Initial data

--- data-file.js
+++ data-file.js
@@ -1,13 +1,439 @@
 // Data File
 
-var create_date = new Date("03-16-2024");
+var create_date = "16-Mar-2024"
 
 var raga_data = [
+
     {
-        "mk_num":"1",
-        "raga":"Kanakangi",
-        "notes":"R1G2 D2N2",
-        "example":"Shree",
-        "pratim":"Barati"
-    }
+        "raga": "Kanakaangi (1)",
+        "notes": "R1G1-M1P-D1N1",
+        "example": "",
+        "related": "Saalagam (37)"
+      },
+      {
+        "raga": "Rathnaangi (2)",
+        "notes": "R1G1-M1P-D1N2",
+        "example": "",
+        "related": "Jalaarnavam (38)"
+      },
+      {
+        "raga": "Gaanamoorthi (3)",
+        "notes": "R1G1-M1P-D1N3",
+        "example": "",
+        "related": "Jhaalavaraali (39)"
+      },
+      {
+        "raga": "Vanaspathi (4)",
+        "notes": "R1G1-M1P-D2N2",
+        "example": "",
+        "related": "Navaneetham (40)"
+      },
+      {
+        "raga": "Maanavathi (5)",
+        "notes": "R1G1-M1P-D2N3",
+        "example": "",
+        "related": "Paavani (41)"
+      },
+      {
+        "raga": "Thaanaroopi (6)",
+        "notes": "R1G1-M1P-D3N3",
+        "example": "",
+        "related": "Raghupriya (42)"
+      },
+      {
+        "raga": "Senaavathi (7)",
+        "notes": "R1G2-M1P-D1N1",
+        "example": "",
+        "related": "Ghavambodhi (43)"
+      },
+      {
+        "raga": "Hanumathodi (Thodi) (8)",
+        "notes": "R1G2-M1P-D1N2",
+        "example": "Thaye Yashoda",
+        "related": "Bhaavapriya (44)"
+      },
+      {
+        "raga": "Dhenuka (9)",
+        "notes": "R1G2-M1P-D1N3",
+        "example": "TeliyalEru rAmA",
+        "related": "Shubhapantuvarali (45)"
+      },
+      {
+        "raga": "Naatakapriya (10)",
+        "notes": "R1G2-M1P-D2N2",
+        "example": "Pibare Ramasrasam",
+        "related": "Shadvidhamargini (46)"
+      },
+      {
+        "raga": "Kokilapriya (11)",
+        "notes": "R1G2-M1P-D2N3",
+        "example": "",
+        "related": "Suvrnaangi (47)"
+      },
+      {
+        "raga": "Roopavathi (12)",
+        "notes": "R1G2-M1P-D3N3",
+        "example": "",
+        "related": "Divyamani (48)"
+      },
+      {
+        "raga": "Gaayakapriya (13)",
+        "notes": "R1G3-M1P-D1N1",
+        "example": "",
+        "related": "Dhavalambaadri (49)"
+      },
+      {
+        "raga": "VakuLaabharana (14)",
+        "notes": "R1G3-M1P-D1N2",
+        "example": "",
+        "related": "Namanaaraayani (50)"
+      },
+      {
+        "raga": "MaayaamaalavaGowla (15)",
+        "notes": "R1G3-M1P-D1N3",
+        "example": "",
+        "related": "Pantuvarali(51)"
+      },
+      {
+        "raga": "Chakravaaka (16)",
+        "notes": "R1G3-M1P-D2N2",
+        "example": "\"Sugunamulem",
+        "related": " KoosinaKandeera\""
+      },
+      {
+        "raga": "Sooryakaantha (17)",
+        "notes": "R1G3-M1P-D2N3",
+        "example": "",
+        "related": "Gamanashrama (53)"
+      },
+      {
+        "raga": "Hatakambari (18)",
+        "notes": "R1G3-M1P-D3N3",
+        "example": "",
+        "related": "Vishvambhari (54)"
+      },
+      {
+        "raga": "Jhankaaradwani (19)",
+        "notes": "R2G2-M1P-D1N1",
+        "example": "",
+        "related": "Shyamalangi (55)"
+      },
+      {
+        "raga": "Natabhairavi (20)",
+        "notes": "R2G2-M1P-D1N2",
+        "example": "Sri valli devasena pate",
+        "related": "Shanmukhapriya (56)"
+      },
+      {
+        "raga": "Keeravaani (21)",
+        "notes": "R2G2-M1P-D1N3",
+        "example": "Ambavaani",
+        "related": "Simhendramadyamam (57)"
+      },
+      {
+        "raga": "Kharaharapriya (22)",
+        "notes": "R2G2-M1P-D2N2",
+        "example": "\"Chakkani Raja",
+        "related": " Pakkala Nilabadi\""
+      },
+      {
+        "raga": "Gowrimanohari (23)",
+        "notes": "R2G2-M1P-D2N3",
+        "example": "Guruleka etuvanti",
+        "related": "Dharmavathi (59)"
+      },
+      {
+        "raga": "Varunapriya (24)",
+        "notes": "R2G2-M1P-D3N3",
+        "example": "",
+        "related": "Neethimathi (60)"
+      },
+      {
+        "raga": "Maararanjani (25)",
+        "notes": "R2G3-M1P-D1N1",
+        "example": "",
+        "related": "Kanthamani (61)"
+      },
+      {
+        "raga": "Chaarukesi (26)",
+        "notes": "R2G3-M1P-D1N2",
+        "example": "Adamodi Galade",
+        "related": "Rishabhapriya (62)"
+      },
+      {
+        "raga": "Sarasaangi (27)",
+        "notes": "R2G3-M1P-D1N3",
+        "example": "",
+        "related": "Lathangi (63)"
+      },
+      {
+        "raga": "Harikambhoji (28)",
+        "notes": "R2G3-M1P-D2N2",
+        "example": "\"O Rangasai",
+        "related": " Enduku Nirdaya"
+      },
+      {
+        "raga": "Dheerashankarabharana (29)",
+        "notes": "R2G3-M1P-D2N3",
+        "example": "\"Sarojadala Netri",
+        "related": " Pogadirelo Ranga\""
+      },
+      {
+        "raga": "Naaganandini (30)",
+        "notes": "R2G3-M1P-D3N3",
+        "example": "",
+        "related": "Chitrambari (66)"
+      },
+      {
+        "raga": "Yogapriya (31)",
+        "notes": "R3G3-M1P-D1N1",
+        "example": "",
+        "related": "Sucharitra (67)"
+      },
+      {
+        "raga": "Raagavardhini (32)",
+        "notes": "R3G3-M1P-D1N2",
+        "example": "",
+        "related": "Jyothiswaroopini (68)"
+      },
+      {
+        "raga": "Gangyabhushini (33)",
+        "notes": "R3G3-M1P-D1N3",
+        "example": "",
+        "related": "Dhatuvardhini (69)"
+      },
+      {
+        "raga": "Vaagadheeshwari (34)",
+        "notes": "R3G3-M1P-D2N2",
+        "example": "",
+        "related": "Naasikabhooshini (70)"
+      },
+      {
+        "raga": "Shulini (35)",
+        "notes": "R3G3-M1P-D2N3",
+        "example": "",
+        "related": "Kosalam (71)"
+      },
+      {
+        "raga": "Chalanata (36)",
+        "notes": "R3G3-M1P-D3N3",
+        "example": "",
+        "related": "Rasikapriya (72)"
+      },
+      {
+        "raga": "Saalagam (37)",
+        "notes": "R1G1-M2P-D1N1",
+        "example": "",
+        "related": "Kanakaangi (1)"
+      },
+      {
+        "raga": "Jalaarnavam (38)",
+        "notes": "R1G1-M2P-D1N2",
+        "example": "",
+        "related": "Rathnaangi (2)"
+      },
+      {
+        "raga": "Jhaalavaraali (39)",
+        "notes": "R1G1-M2P-D1N3",
+        "example": "",
+        "related": "Gaanamoorthi (3)"
+      },
+      {
+        "raga": "Navaneetham (40)",
+        "notes": "R1G1-M2P-D2N2",
+        "example": "",
+        "related": "Vanaspathi (4)"
+      },
+      {
+        "raga": "Paavani (41)",
+        "notes": "R1G1-M2P-D2N3",
+        "example": "",
+        "related": "Maanavathi (5)"
+      },
+      {
+        "raga": "Raghupriya (42)",
+        "notes": "R1G1-M2P-D3N3",
+        "example": "",
+        "related": "Thaanaroopi (6)"
+      },
+      {
+        "raga": "Ghavambodhi (43)",
+        "notes": "R1G2-M2P-D1N1",
+        "example": "",
+        "related": "Senaavathi (7)"
+      },
+      {
+        "raga": "Bhaavapriya (44)",
+        "notes": "R1G2-M2P-D1N2",
+        "example": "",
+        "related": "Hanumathodi (Thodi) (8)"
+      },
+      {
+        "raga": "Shubhapantuvarali (45)",
+        "notes": "R1G2-M2P-D1N3",
+        "example": "",
+        "related": "Dhenuka (9)"
+      },
+      {
+        "raga": "Shadvidhamargini (46)",
+        "notes": "R1G2-M2P-D2N2",
+        "example": "",
+        "related": "Naatakapriya (10)"
+      },
+      {
+        "raga": "Suvrnaangi (47)",
+        "notes": "R1G2-M2P-D2N3",
+        "example": "",
+        "related": "Kokilapriya (11)"
+      },
+      {
+        "raga": "Divyamani (48)",
+        "notes": "R1G2-M2P-D3N3",
+        "example": "",
+        "related": "Roopavathi (12)"
+      },
+      {
+        "raga": "Dhavalambaadri (49)",
+        "notes": "R1G3-M2P-D1N1",
+        "example": "",
+        "related": "Gaayakapriya (13)"
+      },
+      {
+        "raga": "Namanaaraayani (50)",
+        "notes": "R1G3-M2P-D1N2",
+        "example": "",
+        "related": "VakuLaabharana (14)"
+      },
+      {
+        "raga": "Pantuvarali(51)",
+        "notes": "R1G3-M2P-D1N3",
+        "example": "\"Shivashiva ena radha",
+        "related": " Guruvina Gulaama\""
+      },
+      {
+        "raga": "Ramapriya (52)",
+        "notes": "R1G3-M2P-D2N2",
+        "example": "kOrina varam",
+        "related": "Chakravaaka (16)"
+      },
+      {
+        "raga": "Gamanashrama (53)",
+        "notes": "R1G3-M2P-D2N3",
+        "example": "",
+        "related": "Sooryakaantha (17)"
+      },
+      {
+        "raga": "Vishvambhari (54)",
+        "notes": "R1G3-M2P-D3N3",
+        "example": "",
+        "related": "Hatakambari (18)"
+      },
+      {
+        "raga": "Shyamalangi (55)",
+        "notes": "R2G2-M2P-D1N1",
+        "example": "",
+        "related": "Jhankaaradwani (19)"
+      },
+      {
+        "raga": "Shanmukhapriya (56)",
+        "notes": "R2G2-M2P-D1N2",
+        "example": "\"Marivere",
+        "related": " Siddhi Vinayakam"
+      },
+      {
+        "raga": "Simhendramadyamam (57)",
+        "notes": "R2G2-M2P-D1N3",
+        "example": "\"Needu charanamule",
+        "related": " Ashaindhadum\""
+      },
+      {
+        "raga": "Hemavathi (58)",
+        "notes": "R2G2-M2P-D2N2",
+        "example": "Sree Kanthimathim",
+        "related": "Kharaharapriya (22)"
+      },
+      {
+        "raga": "Dharmavathi (59)",
+        "notes": "R2G2-M2P-D2N3",
+        "example": "",
+        "related": "Gowrimanohari (23)"
+      },
+      {
+        "raga": "Neethimathi (60)",
+        "notes": "R2G2-M2P-D3N3",
+        "example": "",
+        "related": "Varunapriya (24)"
+      },
+      {
+        "raga": "Kanthamani (61)",
+        "notes": "R2G3-M2P-D1N1",
+        "example": "",
+        "related": "Maararanjani (25)"
+      },
+      {
+        "raga": "Rishabhapriya (62)",
+        "notes": "R2G3-M2P-D1N2",
+        "example": "",
+        "related": "Chaarukesi (26)"
+      },
+      {
+        "raga": "Lathangi (63)",
+        "notes": "R2G3-M2P-D1N3",
+        "example": "",
+        "related": "Sarasaangi (27)"
+      },
+      {
+        "raga": "Vachaspathi (64)",
+        "notes": "R2G3-M2P-D2N2",
+        "example": "",
+        "related": "Harikambhoji (28)"
+      },
+      {
+        "raga": "Mechakalyani (65)",
+        "notes": "R2G3-M2P-D2N3",
+        "example": "Ethavunara",
+        "related": "Dheerashankarabharana (29)"
+      },
+      {
+        "raga": "Chitrambari (66)",
+        "notes": "R2G3-M2P-D3N3",
+        "example": "",
+        "related": "Naaganandini (30)"
+      },
+      {
+        "raga": "Sucharitra (67)",
+        "notes": "R3G3-M2P-D1N1",
+        "example": "",
+        "related": "Yogapriya (31)"
+      },
+      {
+        "raga": "Jyothiswaroopini (68)",
+        "notes": "R3G3-M2P-D1N2",
+        "example": "",
+        "related": "Raagavardhini (32)"
+      },
+      {
+        "raga": "Dhatuvardhini (69)",
+        "notes": "R3G3-M2P-D1N3",
+        "example": "",
+        "related": "Gangyabhushini (33)"
+      },
+      {
+        "raga": "Naasikabhooshini (70)",
+        "notes": "R3G3-M2P-D2N2",
+        "example": "",
+        "related": "Vaagadheeshwari (34)"
+      },
+      {
+        "raga": "Kosalam (71)",
+        "notes": "R3G3-M2P-D2N3",
+        "example": "",
+        "related": "Shulini (35)"
+      },
+      {
+        "raga": "Rasikapriya (72)",
+        "notes": "R3G3-M2P-D3N3",
+        "example": "",
+        "related": "Chalanata (36)"
+      }
 ]
