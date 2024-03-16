3ebccffde7b6a543f02c31907dc20fd53b579cbe
Subramanya Hebbar <shebbars@yahoo.com>
2024-03-16 16:35:49 +0530

    Initial commit

---
+++ data-file.js
@@ -0,0 +1,13 @@
+// Data File
+
+var create_date = new Date("03-16-2024");
+
+var raga_data = [
+    {
+        "mk_num":"1",
+        "raga":"Kanakangi",
+        "notes":"R1G2 D2N2",
+        "example":"Shree",
+        "pratim":"Barati"
+    }
+]
